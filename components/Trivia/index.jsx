fcfb55c1f1cbf2c0f445a8d91b3acfd1a6f80353
alstudio93 <83974894+alstudio93@users.noreply.github.com>
2022-09-09 18:52:32 -0500

    Update index.jsx

--- components/Trivia/index.jsx
+++ components/Trivia/index.jsx
@@ -28,7 +28,7 @@ const Trivia = () => {
     const accordionOpen = "block max-h-[400px] overflow-y-visible opacity-100 mr-auto"
     return (
         <>
-            <section id="trivia" tabIndex="0"  className='px-2 pt-10'>
+            <section id="trivia" className='px-2 pt-10'>
                 <h2 className='text-transparent bg-clip-text bg-gradient-to-br
                 from-[#e1e1e1] to-[#00a0c4] font-quicksand text-h1
                 text-center font-medium'>Schitt&#39;s Creek Trivia</h2>
