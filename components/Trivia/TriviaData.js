9080592c88525263c0190b0e33ca1ab2b4ca3dfe
alstudio93 <83974894+alstudio93@users.noreply.github.com>
2022-09-09 18:34:13 -0500

    Update and rename TriviaArray.js to TriviaData.js

---
+++ components/Trivia/TriviaData.js
@@ -0,0 +1,32 @@
+export const TriviaQuestion = [
+    {
+        question: "What is the last name of Johnny, Moira, David, and Alexis?",
+        answer: "Rose",
+        headingID: "item-1-header",
+        answerID: "item-1-answer"
+    },
+    {
+        question: "David is played by what actor? ",
+        answer: "Dan Levy",
+        headingID: "item-2-header",
+        answerID: "item-2-answer"
+    },
+    {
+        question: "What is the name of the motel Johnny Rose owns? ",
+        answer: "Rosebud Motel",
+        headingID: "item-3-header",
+        answerID: "item-3-answer"
+    },
+    {
+        question: "How much money did Johnny Rose invest in his video rental business initially?",
+        answer: "$2,000",
+        headingID: "item-4-header",
+        answerID: "item-4-answer"
+    },
+    {
+        question: "What's the name of the soap opera that Moira Rose is best known for?",
+        answer: "Sunrise Bay",
+        headingID: "item-5-header",
+        answerID: "item-5-answer"
+    },
+]
