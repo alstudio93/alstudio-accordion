90a6e6d3aa2b4d9f4e0bf81fe967e180c4409e33
Ladd1993 <aladd1993@gmail.com>
2022-09-10 23:13:28 -0500

    First Commit

--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -3,26 +3,9 @@ import { useTheme } from 'next-themes';
 import { HiOutlineSun } from 'react-icons/hi'
 import { MdOutlineDarkMode } from 'react-icons/md'
 const ThemeSwitcher = () => {
-    const { resolvedTheme, setTheme } = useTheme()
-    const [onComponentMount, setOnComponentMount] = useState(false);
-
-    // After mounting, we have access to the theme
-    useEffect(() => setOnComponentMount(true), []);
-    const themeIcon = () => {
-        if (onComponentMount && resolvedTheme === "dark") {
-            return <HiOutlineSun className='lg:text-2xl' />
-        } else if(onComponentMount && resolvedTheme === "light") {
-            return <MdOutlineDarkMode className='lg:text-2xl'  />
-        }
-    }
+    
     return (
-        <button
-            aria-label={resolvedTheme === "light" ? "Press enter to switch to Dark Mode" : "Press enter to switch to Light Mode"}
-            type="button"
-            className="flex items-center justify-center w-8 h-8 mx-auto transition-all bg-gray-200 rounded-lg md:w-12 md:h-12 dark:bg-gray-600 hover:ring-2 ring-gray-300 "
-            onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-        >
-            {themeIcon()}
+        <button>
         </button>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Trivia from '../components/Trivia'
 import Footer from '../components/Footer'
 import ThemeSwitcher from '../components/ThemeSwitcher'
@@ -7,15 +6,14 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>ALStudio Accordian</title>
-        <meta name="description" content="Accordian designed and developed by ALStudio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className='py-20'>
+
+        {/* Light and Dark Mode Toggle */}
         <ThemeSwitcher />
+
+        {/* Trivia Component */}
         <Trivia />
+
       </main>
       <Footer />
     </>
