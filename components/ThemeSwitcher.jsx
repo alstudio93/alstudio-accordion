7a920173dfae1ca3bfff4c58ec4db15573046109
Ladd1993 <aladd1993@gmail.com>
2022-09-12 19:23:53 -0500

    boilerplate

--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -2,15 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes';
 import { HiOutlineSun } from 'react-icons/hi'
 import { MdOutlineDarkMode } from 'react-icons/md'
-
 const ThemeSwitcher = () => {
-    
+
     return (
         <button
             type="button"
-            className="theme-switcher"
+            className="theme-switcher "
         >
-            Toggle
+            {/* Icons */}
         </button>
     )
 }
--- components/Trivia/index.jsx
+++ components/Trivia/index.jsx
@@ -7,30 +7,26 @@ const Trivia = () => {
     return (
         <>
             <section id="trivia">
-                <h2 >Schitt&#39;s Creek Trivia</h2>
+                <h2>Schitt&#39;s Creek Trivia</h2>
                 {
                     TriviaQuestion.map((trivia, index) => (
                         <div key={trivia.question} className='accordion'>
                             <div className='accordion-question'>
-
                                 <h3>
                                     <button>{trivia.question}</button>
                                 </h3>
-
                                 <span>
-                                    Icons
+                                    {/* Icons */}
                                 </span>
-
                             </div>
                             {
-                                <div 
-                                    // className={isClicked === index ? "accordion-open" : "accordion-closed"}
-                                >
-                                    <p>{trivia.answer}</p>
-                                </div>
+                                <section>
+                                    <p className='accordion-answer'>{trivia.answer}</p>
+                                </section>
                             }
                         </div>
-                ))}
+                    ))
+                }
             </section>
         </>
     )
